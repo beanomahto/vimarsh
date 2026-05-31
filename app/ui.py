d0aa750ad6275d918782f159e126afaa9630ab22
beanomahto <beanomahto9031@gmail.com>
2026-05-31 16:41:13 +0000

    last_fix2

--- app/ui.py
+++ app/ui.py
@@ -13,7 +13,8 @@ from app.chat_history import (
 )
 
 # ── Configurable API URL (for deployment) ────────
-API_URL = os.getenv("API_URL", "http://localhost:8000")
+# API_URL = os.getenv("API_URL", "http://localhost:8000")
+API_URL = os.getenv("API_URL", "https://vimarsh-l5x7.onrender.com")
 
 st.set_page_config(page_title="RAG Chatbot", page_icon="🤖", layout="wide")
 st.title("RAG Chatbot")
